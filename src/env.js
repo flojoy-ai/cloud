ed28765dbdd328c1dcad3389b66098924d72fef4
Joey Yu <joey@jyu.dev>
2023-12-26 22:26:55 -0500

    chore: update env

--- src/env.js
+++ src/env.js
@@ -7,13 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL",
-      ),
+    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
