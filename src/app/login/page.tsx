a312d9426d5c90f90acaf6a56fc488de58b5bcd3
Joey Yu <joey@jyu.dev>
2023-12-27 16:13:28 -0500

    feat: better login signup pagae style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,44 @@
-export default async function Login() {
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+export default async function LogIn() {
   return (
     <div>
-      <div>Log In</div>
-      <a href="/login/auth0">Auth0</a>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Log in to your account
+            </h1>
+            {/* <p className="text-muted-foreground text-sm"> */}
+            {/*   Enter your email below to create your account */}
+            {/* </p> */}
+          </div>
+
+          {/* TODO: Update this to continue with Google */}
+          <Button asChild>
+            <Link href="/login/auth0">Continue</Link>
+          </Button>
+
+          <p className="text-muted-foreground px-8 text-center text-sm">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="hover:text-primary underline underline-offset-4"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="hover:text-primary underline underline-offset-4"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,44 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
 export default async function SignUp() {
-  return <div>Sign Up</div>;
+  return (
+    <div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            {/* <p className="text-muted-foreground text-sm"> */}
+            {/*   Enter your email below to create your account */}
+            {/* </p> */}
+          </div>
+
+          {/* TODO: Update this to continue with Google */}
+          <Button asChild>
+            <Link href="/login/auth0">Continue</Link>
+          </Button>
+
+          <p className="text-muted-foreground px-8 text-center text-sm">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="hover:text-primary underline underline-offset-4"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="hover:text-primary underline underline-offset-4"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
