66c382b652401a6841e37c1b84527a6f1bff2aea
Joey Yu <joey@jyu.dev>
2023-12-28 10:36:00 -0500

    fix: hmr db conn err

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,11 @@
 import { env } from "~/env";
 import * as schema from "./schema";
+import { remember } from "@epic-web/remember";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 export const client = postgres(env.DATABASE_URL);
 
-export const db = drizzle(client, { schema });
+// remember creates a singleton to fix too many db connection err with HMR
+export const db = remember("db", () => drizzle(client, { schema }));
