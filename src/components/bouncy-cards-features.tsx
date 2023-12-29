af9bb46faa45a1c38dcbf46bc0d6b6a67134e50f
Joey Yu <joey@jyu.dev>
2023-12-28 22:35:33 -0500

    fix: card color dark mode

--- src/components/bouncy-cards-features.tsx
+++ src/components/bouncy-cards-features.tsx
@@ -5,16 +5,16 @@ import { motion } from "framer-motion";
 
 export const BouncyCardsFeatures = () => {
   return (
-    <section className="mx-auto max-w-7xl px-4 py-12 text-slate-800">
+    <section className="mx-auto max-w-7xl px-4 py-12 ">
       <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-8">
-        <h2 className="max-w-lg text-4xl font-bold md:text-5xl">
+        <h2 className="max-w-lg text-4xl font-bold text-muted-foreground md:text-5xl">
           Analyze faster with our
-          <span className="text-slate-400"> all in one solution</span>
+          <span className="text-primary"> all in one solution</span>
         </h2>
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
+          className="whitespace-nowrap rounded-lg bg-primary px-4 py-2 font-medium text-primary-foreground shadow-xl transition-colors hover:bg-primary/90"
         >
           Learn more
         </motion.button>
@@ -69,7 +69,7 @@ const BounceCard = ({
   return (
     <motion.div
       whileHover={{ scale: 0.95, rotate: "-1deg" }}
-      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-100 p-8 ${className}`}
+      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-secondary p-8 ${className}`}
     >
       {children}
     </motion.div>
