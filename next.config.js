078f96cdeb66b2555c00aef369808b928f595b0f
Joey Yu <joey@jyu.dev>
2023-12-29 12:11:39 -0500

    chore: bring in bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 await import("./src/env.js");
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default bundleAnalyzer(config);
