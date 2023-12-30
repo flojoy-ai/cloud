634c6b60544d16f9ca8d64209207bbadd5c575b3
Joey Yu <joey@jyu.dev>
2023-12-29 22:27:36 -0500

    chore: update marketing text

--- src/components/bouncy-cards-features.tsx
+++ src/components/bouncy-cards-features.tsx
@@ -21,7 +21,7 @@ export const BouncyCardsFeatures = () => {
       </div>
       <div className="mb-4 grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>Do a thing</CardTitle>
+          <CardTitle>Register your tests and hardwares</CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-200 group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-indigo-50">
               FEATURE DEMO HERE
@@ -29,7 +29,9 @@ export const BouncyCardsFeatures = () => {
           </div>
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>Do another thing</CardTitle>
+          <CardTitle>
+            Stream data in via our Python client or REST API
+          </CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-200 group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-orange-50">
               FEATURE DEMO HERE
@@ -39,7 +41,9 @@ export const BouncyCardsFeatures = () => {
       </div>
       <div className="grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>And this too</CardTitle>
+          <CardTitle>
+            Create powerful visualizations and discover insights
+          </CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-200 group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-emerald-50">
               FEATURE DEMO HERE
@@ -47,7 +51,7 @@ export const BouncyCardsFeatures = () => {
           </div>
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>And finally this</CardTitle>
+          <CardTitle>Generate beautiful reports</CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-200 group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-red-50">
               FEATURE DEMO HERE
