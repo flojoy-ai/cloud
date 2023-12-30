ed8d489faf79a8ea38771a308553df69f22d0d5f
Joey Yu <joey@jyu.dev>
2023-12-30 12:48:07 -0500

    fix: build type error

--- src/types/measurement.ts
+++ src/types/measurement.ts
@@ -1,13 +1,7 @@
 import { measurement } from "~/server/db/schema";
-import { z } from "zod";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export type SelectMeasurement = typeof measurement.$inferSelect;
 
-export const insertMeasurementSchema = createInsertSchema(measurement, {
-  // TODO: this is a drizzle bug, waiting for fix
-  // https://github.com/drizzle-team/drizzle-orm/issues/1110
-  // https://github.com/drizzle-team/drizzle-orm/issues/1609
-  tags: z.array(z.string()).default([]),
-});
+export const insertMeasurementSchema = createInsertSchema(measurement);
 export const selectMeasurementSchema = createSelectSchema(measurement);
