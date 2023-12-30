44b975c5b2d3a6b407c2a266ddb4e74418cdff2a
Joey Yu <joey@jyu.dev>
2023-12-29 20:02:33 -0500

    feat: persist explorer selections

--- src/store/explorer.ts
+++ src/store/explorer.ts
@@ -3,6 +3,7 @@ import { type SelectDevice } from "~/types/device";
 import { type SelectProject } from "~/types/project";
 import { type SelectTest } from "~/types/test";
 import { type SelectWorkspace } from "~/types/workspace";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export interface ExplorerStore {
   selectedWorkspace: SelectWorkspace | undefined;
@@ -15,16 +16,25 @@ export interface ExplorerStore {
   setSelectedTest: (test: SelectTest | undefined) => void;
 }
 
-export const useExplorerStore = create<ExplorerStore>((set) => ({
-  selectedWorkspace: undefined,
-  setSelectedWorkspace: (workspace) =>
-    set((state) => ({ ...state, selectedWorkspace: workspace })),
-  selectedProject: undefined,
-  setSelectedProject: (project) =>
-    set((state) => ({ ...state, selectedProject: project })),
-  selectedDevice: undefined,
-  setSelectedDevice: (device) =>
-    set((state) => ({ ...state, selectedDevice: device })),
-  selectedTest: undefined,
-  setSelectedTest: (test) => set((state) => ({ ...state, selectedTest: test })),
-}));
+export const useExplorerStore = create<ExplorerStore>()(
+  persist(
+    (set) => ({
+      selectedWorkspace: undefined,
+      setSelectedWorkspace: (workspace) =>
+        set((state) => ({ ...state, selectedWorkspace: workspace })),
+      selectedProject: undefined,
+      setSelectedProject: (project) =>
+        set((state) => ({ ...state, selectedProject: project })),
+      selectedDevice: undefined,
+      setSelectedDevice: (device) =>
+        set((state) => ({ ...state, selectedDevice: device })),
+      selectedTest: undefined,
+      setSelectedTest: (test) =>
+        set((state) => ({ ...state, selectedTest: test })),
+    }),
+    {
+      name: "explorer-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
