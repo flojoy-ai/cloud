cf10c1ad61c2e0b9378385a689270b2728fa4c5a
Joey Yu <joey@jyu.dev>
2024-01-02 14:05:56 -0500

    chore: user placeholder

--- src/app/(protected)/workspace/[workspaceId]/users/page.tsx
+++ src/app/(protected)/workspace/[workspaceId]/users/page.tsx
@@ -1,5 +1,17 @@
+import { Separator } from "~/components/ui/separator";
+
 function UserPage() {
-  return <div>UserPage</div>;
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Users</h3>
+        <p className="text-sm text-muted-foreground">
+          Manage users in the current workspace.
+        </p>
+      </div>
+      <Separator />
+    </div>
+  );
 }
 
 export default UserPage;
