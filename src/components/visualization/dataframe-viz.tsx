d0fd92a29fd8eb974e7fc8761a8c249fba9cf7df
Joey Yu <joey@jyu.dev>
2024-01-02 17:39:11 -0500

    style: move form one level up

--- src/components/visualization/dataframe-viz.tsx
+++ src/components/visualization/dataframe-viz.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import LinePlot from "~/components/visualization/plot/line-plot";
 import { type SelectTest } from "~/types/test";
 import { type SelectMeasurement } from "~/types/measurement";
@@ -55,188 +61,189 @@ const DataFrameViz = ({
         <CardHeader>
           <CardTitle>Visualization Options</CardTitle>
         </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="grid grid-cols-1 gap-4 lg:grid-cols-3"
-            >
-              <FormField
-                control={form.control}
-                name="upperControlLimit"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        Upper control limit ={" "}
-                      </FormLabel>
-                      <FormControl className="w-40">
-                        <Input
-                          {...field}
-                          type="number"
-                          step="any"
-                          {...form.register("upperControlLimit", {
-                            setValueAs: (val: string) =>
-                              val !== "" ? parseFloat(val) : undefined,
-                          })}
-                        />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="lowerControlLimit"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        Lower control limit ={" "}
-                      </FormLabel>
-                      <FormControl className="w-40">
-                        <Input
-                          {...field}
-                          type="number"
-                          step="any"
-                          {...form.register("lowerControlLimit", {
-                            setValueAs: (val: string) =>
-                              val !== "" ? parseFloat(val) : undefined,
-                          })}
-                        ></Input>
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="yTransform"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        yT ={" "}
-                      </FormLabel>
-                      <FormControl className="w-40">
-                        <Input {...field}></Input>
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="upperControlLimitTransform"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        UCL_T ={" "}
-                      </FormLabel>
-                      <FormControl className="w-40">
-                        <Input {...field}></Input>
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="lowerControlLimitTransform"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        LCL_T ={" "}
-                      </FormLabel>
-                      <FormControl className="w-40">
-                        <Input {...field}></Input>
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="logScaleYAxis"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        Log y-axis
-                      </FormLabel>
-                      <FormControl>
-                        <Checkbox
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="errorBars"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex items-center gap-x-2">
-                      <FormLabel className="font-bold text-muted-foreground">
-                        Error bars
-                      </FormLabel>
-                      <FormControl>
-                        <Checkbox
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="errorPercentage"
-                render={({ field }) =>
-                  form.watch("errorBars") ? (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardContent>
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                <FormField
+                  control={form.control}
+                  name="upperControlLimit"
+                  render={({ field }) => (
                     <FormItem>
                       <div className="flex items-center gap-x-2">
-                        <FormControl>
+                        <FormLabel className="font-bold text-muted-foreground">
+                          Upper control limit ={" "}
+                        </FormLabel>
+                        <FormControl className="w-40">
                           <Input
+                            {...field}
                             type="number"
                             step="any"
-                            className="h-8 w-16 p-2"
-                            {...field}
-                            {...form.register("errorPercentage", {
+                            {...form.register("upperControlLimit", {
                               setValueAs: (val: string) =>
                                 val !== "" ? parseFloat(val) : undefined,
                             })}
                           />
                         </FormControl>
-                        <FormLabel className="font-bold text-muted-foreground">
-                          %
-                        </FormLabel>
                       </div>
                       <FormMessage />
                     </FormItem>
-                  ) : (
-                    <div className="py-4" />
-                  )
-                }
-              />
-              <div className="py-1" />
-              <Button type="submit">Replot</Button>
-            </form>
-          </Form>
-        </CardContent>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lowerControlLimit"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          Lower control limit ={" "}
+                        </FormLabel>
+                        <FormControl className="w-40">
+                          <Input
+                            {...field}
+                            type="number"
+                            step="any"
+                            {...form.register("lowerControlLimit", {
+                              setValueAs: (val: string) =>
+                                val !== "" ? parseFloat(val) : undefined,
+                            })}
+                          ></Input>
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="yTransform"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          yT ={" "}
+                        </FormLabel>
+                        <FormControl className="w-40">
+                          <Input {...field}></Input>
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="upperControlLimitTransform"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          UCL_T ={" "}
+                        </FormLabel>
+                        <FormControl className="w-40">
+                          <Input {...field}></Input>
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lowerControlLimitTransform"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          LCL_T ={" "}
+                        </FormLabel>
+                        <FormControl className="w-40">
+                          <Input {...field}></Input>
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="logScaleYAxis"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          Log y-axis
+                        </FormLabel>
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="errorBars"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center gap-x-2">
+                        <FormLabel className="font-bold text-muted-foreground">
+                          Error bars
+                        </FormLabel>
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="errorPercentage"
+                  render={({ field }) =>
+                    form.watch("errorBars") ? (
+                      <FormItem>
+                        <div className="flex items-center gap-x-2">
+                          <FormControl>
+                            <Input
+                              type="number"
+                              step="any"
+                              className="h-8 w-16 p-2"
+                              {...field}
+                              {...form.register("errorPercentage", {
+                                setValueAs: (val: string) =>
+                                  val !== "" ? parseFloat(val) : undefined,
+                              })}
+                            />
+                          </FormControl>
+                          <FormLabel className="font-bold text-muted-foreground">
+                            %
+                          </FormLabel>
+                        </div>
+                        <FormMessage />
+                      </FormItem>
+                    ) : (
+                      <div className="py-4" />
+                    )
+                  }
+                />
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <Button type="submit">Plot</Button>
+            </CardFooter>
+          </form>
+        </Form>
       </Card>
       {everythingSelected && measurements && (
         <LinePlot
