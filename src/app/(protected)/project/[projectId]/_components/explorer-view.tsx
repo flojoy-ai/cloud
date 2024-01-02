155d93d878c0cc9cbe2a64f0fb100eadf794e642
Joey Yu <joey@jyu.dev>
2024-01-01 20:20:09 -0500

    style: surround with a Card

--- src/app/(protected)/project/[projectId]/_components/explorer-view.tsx
+++ src/app/(protected)/project/[projectId]/_components/explorer-view.tsx
@@ -41,63 +41,66 @@ const ExplorerView = ({ project }: Props) => {
     <div>
       <div className="py-1"></div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <Card>
-          <CardHeader>
-            <CardTitle>Select test</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <TestCombobox tests={tests ?? []} />
-          </CardContent>
-          <CardFooter>
-            {tests?.length === 0 && (
-              <div>There is no test in the selected project</div>
-            )}
-            {selectedTest && <Badge>{selectedTest.measurementType}</Badge>}
-          </CardFooter>
+      <div className="flex flex-col gap-4">
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>Select test</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <TestCombobox tests={tests ?? []} />
+            </CardContent>
+            <CardFooter>
+              {tests?.length === 0 && (
+                <div>There is no test in the selected project</div>
+              )}
+              {selectedTest && <Badge>{selectedTest.measurementType}</Badge>}
+            </CardFooter>
+          </Card>
+        </div>
+        <Card className="p-2">
+          {everythingSelected && measurements && (
+            <ScatterPlot
+              title={selectedTest?.name ?? "Untitled Test"}
+              x={
+                measurements.map(
+                  (measurement) => measurement.deviceId ?? "Untitled Device",
+                ) ?? []
+              }
+              y={
+                measurements.map((measurement) => {
+                  if (measurement.data.type === "boolean") {
+                    return measurement.data.passed ? "passed" : "failed";
+                  }
+                  return "";
+                }) ?? []
+              }
+            />
+          )}
+
+          {everythingSelected && measurements && (
+            <LinePlot
+              title={selectedTest?.name ?? "Untitled Test"}
+              // lines={[
+              //   { x: [1, 2, 3], y: [1, 2, 3] },
+              //   { x: [3, 2, 1], y: [1, 2, 3] },
+              // ]}
+              lines={
+                measurements.map((measurement) => {
+                  if (measurement.data.type === "dataframe") {
+                    return {
+                      x: measurement.data.dataframe.x ?? [],
+                      y: measurement.data.dataframe.y ?? [],
+                      name: measurement.deviceId,
+                    };
+                  }
+                  return { x: [], y: [], name: "" };
+                }) ?? []
+              }
+            />
+          )}
         </Card>
       </div>
-
-      {everythingSelected && measurements && (
-        <ScatterPlot
-          title={selectedTest?.name ?? "Untitled Test"}
-          x={
-            measurements.map(
-              (measurement) => measurement.deviceId ?? "Untitled Device",
-            ) ?? []
-          }
-          y={
-            measurements.map((measurement) => {
-              if (measurement.data.type === "boolean") {
-                return measurement.data.passed ? "passed" : "failed";
-              }
-              return "";
-            }) ?? []
-          }
-        />
-      )}
-
-      {everythingSelected && measurements && (
-        <LinePlot
-          title={selectedTest?.name ?? "Untitled Test"}
-          // lines={[
-          //   { x: [1, 2, 3], y: [1, 2, 3] },
-          //   { x: [3, 2, 1], y: [1, 2, 3] },
-          // ]}
-          lines={
-            measurements.map((measurement) => {
-              if (measurement.data.type === "dataframe") {
-                return {
-                  x: measurement.data.dataframe.x ?? [],
-                  y: measurement.data.dataframe.y ?? [],
-                  name: measurement.deviceId,
-                };
-              }
-              return { x: [], y: [], name: "" };
-            }) ?? []
-          }
-        />
-      )}
     </div>
   );
 };
