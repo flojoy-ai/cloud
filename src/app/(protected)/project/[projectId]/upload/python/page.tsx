5196edcdcc69d43ed0961d8224330de1805857b1
Joey Yu <joey@jyu.dev>
2024-01-03 09:25:51 -0500

    WIP: feat: example code

--- src/app/(protected)/project/[projectId]/upload/python/page.tsx
+++ src/app/(protected)/project/[projectId]/upload/python/page.tsx
@@ -1,7 +1,31 @@
+"use client";
 import { Separator } from "~/components/ui/separator";
+import SyntaxHighlighter from "react-syntax-highlighter";
+// import { useTheme } from "next-themes";
+// import {
+//   oneDark,
+//   oneLight,
+// } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-const UploadView = async ({ params }: { params: { projectId: string } }) => {
-  console.log(params.projectId);
+const code = `from flojoy_cloud import FlojoyCloud
+
+client = FlojoyCloud(api_key="YOUR_API_KEY")
+
+# test_name and device_name are both unique per project
+
+test = client.get_test(test_name="PASS/FAIL Test", create_if_not_exist=True)
+
+device = client.get_device(device_name="Circuit Board #1", create_if_not_exist=False)
+
+data = {"passed": True}
+
+client.upload(data, test, device)
+`;
+
+const UploadView = ({ params }: { params: { projectId: string } }) => {
+  // const { theme } = useTheme();
+  // if (!theme) return null;
+
   return (
     <div className="space-y-6">
       <div>
@@ -11,6 +35,7 @@ const UploadView = async ({ params }: { params: { projectId: string } }) => {
         </p>
       </div>
       <Separator />
+      <SyntaxHighlighter language="python">{code}</SyntaxHighlighter>
     </div>
   );
 };
