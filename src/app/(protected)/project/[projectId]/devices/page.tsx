e1fad927ab9e724a2082e6be8de8f697fbe45fba
Joey Yu <joey@jyu.dev>
2024-01-04 15:44:54 -0500

    chore: more description

--- src/app/(protected)/project/[projectId]/devices/page.tsx
+++ src/app/(protected)/project/[projectId]/devices/page.tsx
@@ -1,6 +1,7 @@
 import { api } from "~/trpc/server";
 import CreateDevice from "./_components/create-device";
 import AllDevices from "./_components/all-devices";
+import { Separator } from "~/components/ui/separator";
 
 const DevicesView = async ({ params }: { params: { projectId: string } }) => {
   const project = await api.project.getProjectById.query({
@@ -9,6 +10,14 @@ const DevicesView = async ({ params }: { params: { projectId: string } }) => {
 
   return (
     <div>
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Hardware Devices</h2>
+        <p className="text-muted-foreground">
+          Here you can register each individual device you are testing in this
+          project.
+        </p>
+      </div>
+      <Separator className="my-6" />
       <CreateDevice project={project} />
       <div className="py-2"></div>
       <AllDevices project={project} />
--- src/app/(protected)/project/[projectId]/tests/page.tsx
+++ src/app/(protected)/project/[projectId]/tests/page.tsx
@@ -1,6 +1,7 @@
 import CreateTest from "./_components/create-test";
 import AllTests from "./_components/all-tests";
 import { api } from "~/trpc/server";
+import { Separator } from "~/components/ui/separator";
 
 const TestsView = async ({ params }: { params: { projectId: string } }) => {
   const project = await api.project.getProjectById.query({
@@ -8,6 +9,13 @@ const TestsView = async ({ params }: { params: { projectId: string } }) => {
   });
   return (
     <div>
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Test Suite</h2>
+        <p className="text-muted-foreground">
+          Here you can register individual tests into this test suite.
+        </p>
+      </div>
+      <Separator className="my-6" />
       <CreateTest project={project} />
       <div className="py-2"></div>
       <AllTests project={project} />
