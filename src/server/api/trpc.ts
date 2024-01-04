05e4af6cd270a68ba1524a8373be4b1ac64ba38a
Joey Yu <joey@jyu.dev>
2024-01-03 22:37:42 -0500

    feat: support trpc-openpi

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -14,6 +14,7 @@ import { db } from "~/server/db";
 
 import { auth } from "~/auth/lucia";
 import * as context from "next/headers";
+import { type OpenApiMeta } from "trpc-openapi";
 
 /**
  * 1. CONTEXT
@@ -45,19 +46,22 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
  * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
  * errors on the backend.
  */
-const t = initTRPC.context<typeof createTRPCContext>().create({
-  transformer: superjson,
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.cause instanceof ZodError ? error.cause.flatten() : null,
-      },
-    };
-  },
-});
+const t = initTRPC
+  .context<typeof createTRPCContext>()
+  .meta<OpenApiMeta>()
+  .create({
+    transformer: superjson,
+    errorFormatter({ shape, error }) {
+      return {
+        ...shape,
+        data: {
+          ...shape.data,
+          zodError:
+            error.cause instanceof ZodError ? error.cause.flatten() : null,
+        },
+      };
+    },
+  });
 
 /**
  * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
