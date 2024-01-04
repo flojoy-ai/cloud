d37e3c434f7ac0e7033c5ea3ac4958a8748450dc
Joey Yu <joey@jyu.dev>
2024-01-04 17:29:30 -0500

    chore: remvoe import

--- src/app/(protected)/workspace/[workspaceId]/secret/page.tsx
+++ src/app/(protected)/workspace/[workspaceId]/secret/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
