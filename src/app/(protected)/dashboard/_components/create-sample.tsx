b5cdac60509eb537ee33688b90d4df981dbfed85
Joey Yu <joey@jyu.dev>
2024-01-04 17:29:59 -0500

    style: add a space

--- src/app/(protected)/dashboard/_components/create-sample.tsx
+++ src/app/(protected)/dashboard/_components/create-sample.tsx
@@ -37,7 +37,7 @@ const CreateSample = () => {
   const createSample = async () => {
     setIsCreating(true);
     const workspace = await workspaceCreate.mutateAsync({
-      name: "Sample Workspace" + createId(),
+      name: "Sample Workspace " + createId(),
     });
 
     const project = await projectCreate.mutateAsync({
