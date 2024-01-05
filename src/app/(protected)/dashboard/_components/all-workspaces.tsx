9f072b6f5f6c074373aaf2dcc0b244e326df7952
Joey Yu <joey@jyu.dev>
2024-01-04 22:16:53 -0500

    chore: remove some dead code

--- src/app/(protected)/dashboard/_components/all-workspaces.tsx
+++ src/app/(protected)/dashboard/_components/all-workspaces.tsx
@@ -9,19 +9,6 @@ export default async function AllWorkspaces({ workspaces }: Props) {
   return (
     <>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {/* {workspaces */}
-        {/*   .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()) */}
-        {/*   .map((workspace, idx) => { */}
-        {/*     return ( */}
-        {/*       <div key={workspace.id}> */}
-        {/*         {idx !== 0 && <Separator />} */}
-        {/*         <div className="py-2" /> */}
-        {/*         <WorkspaceSection workspace={workspace} /> */}
-        {/*         <div className="py-2" /> */}
-        {/*       </div> */}
-        {/*     ); */}
-        {/*   })} */}
-
         {workspaces
           .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
           .map((workspace) => {
