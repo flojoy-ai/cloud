3418063ef0ed3239bfb99cecab330faeb78b4f0c
Joey Yu <joey@jyu.dev>
2024-01-04 19:53:52 -0500

    feat: pass fail measurement with color

--- src/components/measurement/columns.tsx
+++ src/components/measurement/columns.tsx
@@ -38,6 +38,23 @@ export const columns: ColumnDef<
     accessorKey: "data",
     header: "Data",
     // TODO: implement a better looking preview here
-    accessorFn: (data) => JSON.stringify(data.data).slice(0, 10),
+    accessorFn: (data) => {
+      if (data.data.type === "boolean") {
+        return data.data.passed ? "Passed" : "Failed";
+      }
+
+      return JSON.stringify(data.data).slice(0, 10);
+    },
+    cell: ({ row }) => {
+      switch (row.original.measurementType) {
+        case "boolean":
+          if (row.getValue("data") === "Passed") {
+            return <div className="text-green-500">{row.getValue("data")}</div>;
+          } else if (row.getValue("data") === "Failed") {
+            return <div className="text-red-500">{row.getValue("data")}</div>;
+          }
+      }
+      return <div>{row.getValue("data")}</div>;
+    },
   },
 ];
