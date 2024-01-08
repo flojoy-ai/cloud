5dd2decca4dd5a940e2cd53c57743f21d5213fec
JeffDotPng <zhangjeff2014@gmail.com>
2024-01-08 15:51:38 -0500

    fix checkWorkspaceAccess

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,29 +11,21 @@ export const checkWorkspaceAccess = async (
   ctx: AccessContext,
   workspaceId: string,
 ): Promise<SelectWorkspaceUser | null> => {
-  // There are 2 cases:
-  // Case 1: Authentication with secret key, in this case workspaceId will be
-  // defined in the ctx, thus just need to check if the resource belongs to that
-  // workspace, then we will be done.
+  // Authentication with secret key, the key potentially does not have
+  // access to this workspace
   if (ctx.workspaceId && workspaceId !== ctx.workspaceId) {
     return null;
   }
 
-  // Case 2: Authentication with session, in this case we need to check if the
-  // has access to the workspace that this resource belongs to
-  if (!ctx.workspaceId) {
-    const perm = await ctx.db.query.workspace_user.findFirst({
-      where: (workspace_user, { and, eq }) =>
-        and(
-          eq(workspace_user.workspaceId, workspaceId),
-          eq(workspace_user.userId, ctx.userId),
-        ),
-    });
-    if (!perm) {
-      return null;
-    }
-    return perm;
-  }
+  // Then we need to check if the key/user has access
+  // to the workspace that this resource belongs to
+  const perm = await ctx.db.query.workspace_user.findFirst({
+    where: (workspace_user, { and, eq }) =>
+      and(
+        eq(workspace_user.workspaceId, workspaceId),
+        eq(workspace_user.userId, ctx.userId),
+      ),
+  });
 
-  return null;
+  return perm ?? null;
 };
