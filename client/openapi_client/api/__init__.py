c4f92c596b372d8fea77ec00d198a099c370dcd8
Joey Yu <joey@jyu.dev>
2024-01-09 14:45:50 -0500

    Revert "WIP: feat: generate an example python client"
    This reverts commit 97607649cdf45ae8e3900352985985ecc94789ac.

--- client/openapi_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.device_api import DeviceApi
-from openapi_client.api.measurement_api import MeasurementApi
-from openapi_client.api.project_api import ProjectApi
-from openapi_client.api.test_api import TestApi
-from openapi_client.api.workspace_api import WorkspaceApi
-
