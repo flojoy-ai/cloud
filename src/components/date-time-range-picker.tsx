950646011406a61c309fa723a2f230a8b9e12fca
JeffDotPng <zhangjeff2014@gmail.com>
2024-01-09 16:36:12 -0500

    Add time picker for date range

--- src/components/date-time-range-picker.tsx
+++ src/components/date-time-range-picker.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { format } from "date-fns";
+import { parse, format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { type DateRange } from "react-day-picker";
 
@@ -11,11 +11,24 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
+import { Input } from "./ui/input";
 
 type Props = {
   date: DateRange | undefined;
   setDate: (dates: DateRange | undefined) => void;
 } & React.HTMLAttributes<HTMLDivElement>;
+
+const getDateWithTime = (date: Date, timeStr: string) => {
+  const d = parse(timeStr, "HH:mm:ss", new Date());
+
+  const newDate = new Date(date.getTime());
+
+  newDate.setHours(d.getHours());
+  newDate.setMinutes(d.getMinutes());
+  newDate.setSeconds(d.getSeconds());
+
+  return newDate;
+};
 
 export function DateTimeRangePicker({ className, date, setDate }: Props) {
   return (
@@ -34,11 +47,11 @@ export function DateTimeRangePicker({ className, date, setDate }: Props) {
             {date?.from ? (
               date.to ? (
                 <>
-                  {format(date.from, "LLL dd, y, hh:mm:ss")} -{" "}
-                  {format(date.to, "LLL dd, y, hh:mm:ss")}
+                  {format(date.from, "LLL dd, y, HH:mm:ss")} -{" "}
+                  {format(date.to, "LLL dd, y, HH:mm:ss")}
                 </>
               ) : (
-                format(date.from, "LLL dd, y")
+                format(date.from, "LLL dd, y, HH:mm:ss")
               )
             ) : (
               <span>Pick a time range</span>
@@ -54,6 +67,36 @@ export function DateTimeRangePicker({ className, date, setDate }: Props) {
             onSelect={setDate}
             numberOfMonths={2}
           />
+          <div className="flex justify-between px-20 py-2">
+            <Input
+              type="time"
+              step="1"
+              value={date?.from ? format(date.from, "HH:mm:ss") : undefined}
+              onChange={(e) => {
+                if (date?.from) {
+                  setDate({
+                    ...date,
+                    from: getDateWithTime(date.from, e.target.value),
+                  });
+                }
+              }}
+              className="w-36"
+            />
+            <Input
+              type="time"
+              value={date?.to ? format(date.to, "HH:mm:ss") : undefined}
+              className="w-36"
+              step="1"
+              onChange={(e) => {
+                if (date?.to) {
+                  setDate({
+                    ...date,
+                    to: getDateWithTime(date.to, e.target.value),
+                  });
+                }
+              }}
+            />
+          </div>
         </PopoverContent>
       </Popover>
     </div>
