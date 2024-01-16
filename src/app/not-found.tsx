8a00173fca8de19a4693e7b66e21930d64e3be15
Joey Yu <joey@jyu.dev>
2024-01-16 00:47:54 -0500

    style: remove lg:

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { Button } from "~/components/ui/button";
 function NotFoundPage() {
   return (
     <>
-      <div className="lg:p-8">
+      <div className="p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
