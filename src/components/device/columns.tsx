715640435ea0eaf1d7e4dac8d08718f7f076e1ae
Joey Yu <joey@jyu.dev>
2024-01-16 15:58:49 -0500

    feat: add copy action

--- src/components/device/columns.tsx
+++ src/components/device/columns.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { type ColumnDef } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
 import { type SelectDevice } from "~/types/device";
+import { toast } from "sonner";
 
 export const columns: ColumnDef<SelectDevice>[] = [
   {
@@ -9,15 +22,63 @@ export const columns: ColumnDef<SelectDevice>[] = [
     header: "Name",
   },
   {
-    accessorKey: "id",
-    header: "ID",
-  },
-  {
     accessorKey: "createdAt",
     header: "Created At",
+    cell: ({ row }) => {
+      const [mounted, setMounted] = useState(false);
+      useEffect(() => {
+        setMounted(true);
+      }, []);
+      if (mounted) {
+        return row.original.createdAt.toLocaleString();
+      }
+    },
   },
   {
     accessorKey: "updatedAt",
     header: "Updated At",
+    cell: ({ row }) => {
+      const [mounted, setMounted] = useState(false);
+      useEffect(() => {
+        setMounted(true);
+      }, []);
+      if (mounted) {
+        return row.original.updatedAt?.toLocaleString() ?? "Never";
+      }
+    },
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const device = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                toast.promise(navigator.clipboard.writeText(device.id), {
+                  success: "Copied to clipboard",
+                  error: "Something went wrong :(",
+                })
+              }
+            >
+              Copy device ID
+            </DropdownMenuItem>
+            {/* <DropdownMenuSeparator /> */}
+            {/* <DropdownMenuItem>View customer</DropdownMenuItem> */}
+            {/* <DropdownMenuItem>View payment details</DropdownMenuItem> */}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
