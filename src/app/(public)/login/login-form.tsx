3c3ff02582601925141265f46323cadc908f8b84
Joey Yu <joey@jyu.dev>
2024-01-19 15:14:32 -0500

    chore: client side signup login form

--- src/app/(public)/login/login-form.tsx
+++ src/app/(public)/login/login-form.tsx
@@ -1,20 +1,97 @@
-import Form from "~/components/form";
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 const LoginForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const formData = new FormData();
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+
+    setIsLoading(true);
+    const result = await fetch("/api/login", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (result.status !== 200) {
+      setIsLoading(false);
+
+      form.setError("root", {
+        message: "Log in is not available at the moment :(",
+      });
+    } else {
+      router.push(result.url);
+    }
+  }
+
   return (
-    <Form action="/api/login">
-      <Label htmlFor="email">Email</Label>
-      <Input name="email" id="email" />
-      <div className="py-1"></div>
-      <Label htmlFor="password">Password</Label>
-      <Input type="password" name="password" id="password" />
-      <div className="py-1"></div>
-      <Button className="w-full" type="submit">
-        Log In
-      </Button>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="password" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="text-sm font-medium text-destructive">
+          {form.formState.errors.root?.message}
+        </div>
+        <Button type="submit" disabled={isLoading} className="w-full">
+          {isLoading ? "Loading..." : "Submit"}
+        </Button>
+      </form>
     </Form>
   );
 };
