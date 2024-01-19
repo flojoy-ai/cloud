263c9edadb81f33cd5526825d6c9bfe9c3ce0de4
Joey Yu <joey@jyu.dev>
2024-01-19 14:32:22 -0500

    chore: setup 2 forms

---
+++ src/app/(public)/signup/singup-form.tsx
@@ -0,0 +1,22 @@
+import Form from "~/components/form";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+const SignupForm = () => {
+  return (
+    <Form action="/api/signup">
+      <Label htmlFor="email">Email</Label>
+      <Input name="email" id="email" />
+      <div className="py-1"></div>
+      <Label htmlFor="password">Password</Label>
+      <Input type="password" name="password" id="password" />
+      <div className="py-1"></div>
+      <Button className="w-full" type="submit">
+        Create my account
+      </Button>
+    </Form>
+  );
+};
+
+export default SignupForm;
