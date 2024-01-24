1b3b676023500ee140a57306b3fe5cd57363bb6f
JeffDotPng <zhangjeff2014@gmail.com>
2024-01-24 17:28:35 -0500

    turn off db logging

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -7,4 +7,4 @@ import { drizzle } from "drizzle-orm/neon-serverless";
 export const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 // remember creates a singleton to fix too many db connection err with HMR
-export const db = drizzle(pool, { schema, logger: true });
+export const db = drizzle(pool, { schema });
