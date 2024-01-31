249b37ab64ccb6332d9b61bd4d8fb25a6a18b72e
Joey Yu <joey@jyu.dev>
2024-01-31 13:48:08 -0500

    chore: update user column

--- src/app/(protected)/workspace/[namespace]/settings/users/columns.tsx
+++ src/app/(protected)/workspace/[namespace]/settings/users/columns.tsx
@@ -4,6 +4,18 @@ import { type ColumnDef } from "@tanstack/react-table";
 import { type SelectUser } from "~/types/user";
 import { type SelectWorkspace } from "~/types/workspace";
 import { type SelectWorkspaceUser } from "~/types/workspace_user";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { toast } from "sonner";
 
 export const userColumns: ColumnDef<{
   user: SelectUser;
@@ -23,6 +35,44 @@ export const userColumns: ColumnDef<{
     header: "Role",
     cell: ({ row }) => {
       return <div>{row.original.workspace_user.workspaceRole}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                toast.promise(
+                  navigator.clipboard.writeText(row.original.user.email),
+                  {
+                    success: "Copied to clipboard",
+                    error: "Something went wrong :(",
+                  },
+                )
+              }
+            >
+              Copy email
+            </DropdownMenuItem>
+            {row.original.workspace_user.workspaceRole !== "owner" && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Update role</DropdownMenuItem>
+                <DropdownMenuItem>Remove user</DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
