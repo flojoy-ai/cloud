38aff1a413a1ad4e2b521c531887d171da7429e6
Joey Yu <joey@jyu.dev>
2024-01-31 15:27:02 -0500

    chore: refactor

--- src/app/(protected)/workspace/invites/page.tsx
+++ src/app/(protected)/workspace/invites/page.tsx
@@ -1,16 +1,35 @@
 import { api } from "~/trpc/server";
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "~/components/page-header";
+import { type SelectUserInvite } from "~/types/user";
 
 const InvitePage = async () => {
   const invites = await api.user.getAllWorkspaceInvites.query();
 
   return (
     <div className="container max-w-screen-2xl">
+      <PageHeader>
+        <PageHeaderHeading className="">Workspace Invites</PageHeaderHeading>
+        <PageHeaderDescription>
+          Here are all the pending invites.
+        </PageHeaderDescription>
+      </PageHeader>
+
       {invites.map((invite) => (
-        <div key={invite.id}>
-          <div>{invite.email}</div>
-          <div>{invite.role}</div>
-        </div>
+        <InviteCard key={invite.id} invite={invite} />
       ))}
+    </div>
+  );
+};
+
+const InviteCard = ({ invite }: { invite: SelectUserInvite }) => {
+  return (
+    <div>
+      <div>{invite.email}</div>
+      <div>{invite.role}</div>
     </div>
   );
 };
