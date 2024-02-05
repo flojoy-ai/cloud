df99b0c8f07179c9712a246979e0e943bebbfad8
Joey Yu <joey@jyu.dev>
2024-02-05 16:47:12 -0500

    fix: use new codegen

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,14 +1,14 @@
 import { env } from "~/env";
 
 import { Kysely, PostgresDialect } from "kysely";
-import { type DB } from "kysely-codegen";
 import { Pool } from "pg";
+import Database from "~/schemas/Database";
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
 });
 
-export const db = new Kysely<DB>({
+export const db = new Kysely<Database>({
   dialect: new PostgresDialect({
     pool,
   }),
