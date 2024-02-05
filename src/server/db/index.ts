b46d5a24d26f8e2a4c73a65a4b0219beb47c4353
Joey Yu <joey@jyu.dev>
2024-02-05 16:47:35 -0500

    fix: type import

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,7 @@ import { env } from "~/env";
 
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
-import Database from "~/schemas/Database";
+import type Database from "~/schemas/Database";
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
