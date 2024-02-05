bd3907d49143270d1804bdfaae6ea837b2b5e6a8
Joey Yu <joey@jyu.dev>
2024-02-05 12:20:46 -0500

    chore: example migration

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -1,9 +1,21 @@
 import { type Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  // TODO: move our migrations to here
+  await db.schema
+    .createTable("user")
+    .addColumn("id", "text", (col) => col.primaryKey())
+    .addColumn("email_verified", "boolean", (col) => col.defaultTo(false))
+    .addColumn("email", "text", (col) => col.notNull())
+    .addColumn("hashed_password", "text")
+    .addColumn("created_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn("updated_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  // TODO: move our migrations to here
+  await db.schema.dropTable("user").execute();
 }
