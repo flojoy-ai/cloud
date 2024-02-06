dfeb3630663ed8db0c45b9a3916170f2794a9c00
JeffDotPng <zhangjeff2014@gmail.com>
2024-02-06 17:37:22 -0500

    update test

--- src/types/test.ts
+++ src/types/test.ts
@@ -1,18 +1,11 @@
-// import { testTable } from "~/server/db/schema";
-// import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-//
-// export type SelectTest = typeof testTable.$inferSelect;
-//
-// export const insertTestSchema = createInsertSchema(testTable);
-//
-// export const publicInsertTestSchema = insertTestSchema.pick({
-//   name: true,
-//   projectId: true,
-//   measurementType: true,
-// });
-//
-// export const publicUpdateTestSchema = insertTestSchema.pick({
-//   name: true,
-// });
-//
-// export const selectTestSchema = createSelectSchema(testTable);
+import { test } from "~/schemas/public/Test";
+
+export const insertTestSchema = test.pick({
+  name: true,
+  projectId: true,
+  measurementType: true,
+});
+
+export const updateTestSchema = insertTestSchema.pick({
+  name: true,
+});
