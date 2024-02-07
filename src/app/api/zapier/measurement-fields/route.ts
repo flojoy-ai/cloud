c268a0daed85fa56613413205b0fe51db21706f6
smahmed776 <xsmahmed776@gmail.com>
2024-02-07 19:14:26 +0000

    add tests as childreninside project field

--- src/app/api/zapier/measurement-fields/route.ts
+++ src/app/api/zapier/measurement-fields/route.ts
@@ -19,34 +19,55 @@ export const GET = async (req: NextRequest) => {
     const projects = await api.project.getAllProjectsByWorkspaceId.query({
       workspaceId,
     });
-    const projectFields = projects.map((project) => {
-      return {
+    const projectFields = [];
+    for (const project of projects) {
+      const tests = await api.test.getAllTestsByProjectId.query({
+        projectId: project.id,
+      });
+      projectFields.push({
         key: project.id,
         label: project.name,
         value: project.id,
-      };
-    });
-    let testFields: Array<{ key: string; label: string; value: string }> = ({} =
-      []);
-    if (projects.length > 0) {
-      const tests = await api.test.getAllTestsByProjectId.query({
-        projectId: projects[0]?.id ?? "",
-      });
-      testFields = tests.map((test) => {
-        return {
-          key: test.id,
-          label: test.name,
-          value: test.id,
-        };
+        ...(tests.length > 0 && {
+          children: tests.map((test) => {
+            return {
+              key: test.id,
+              label: test.name,
+              value: test.id,
+            };
+          }),
+        }),
       });
     }
+    // const projectFields = projects.map((project) => {
+    //   const tests = await api.test.getAllTestsByProjectId.query({
+    //     projectId: project.id,
+    //   });
+    //   return {
+    //     key: project.id,
+    //     label: project.name,
+    //     value: project.id,
+    //   };
+    // });
+    // let testFields: Array<{ key: string; label: string; value: string }> = ({} =
+    //   []);
+    // if (projects.length > 0) {
+    //   const tests = await api.test.getAllTestsByProjectId.query({
+    //     projectId: projects[0]?.id ?? "",
+    //   });
+    //   testFields = tests.map((test) => {
+    //     return {
+    //       key: test.id,
+    //       label: test.name,
+    //       value: test.id,
+    //     };
+    //   });
+    // }
+
     return new Response(
       JSON.stringify([
         { key: "hardwareId", label: "Hardware", choices: hardwareFields },
         { key: "projectId", label: "Project", choices: projectFields },
-        ...(testFields.length > 0
-          ? [{ key: "testId", label: "Test", choices: testFields }]
-          : []),
       ]),
       {
         status: 200,
