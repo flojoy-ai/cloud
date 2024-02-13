48650f80364c43b91f15e58aad75d1af105f28af
Joey Yu <joey@jyu.dev>
2024-02-13 11:25:10 -0500

    chore: update schema

--- migrations/0010_revision.ts
+++ migrations/0010_revision.ts
@@ -2,23 +2,28 @@ import { type Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
+    .createType("revision_type")
+    .asEnum(["init", "remove", "add"])
+    .execute();
+
+  await db.schema
     .createTable("hardware_revision")
-    .addColumn("old_hardware_id", "text", (col) =>
+    .addColumn("hardware_id", "text", (col) =>
       col.references("hardware.id").onDelete("cascade").notNull(),
-    )
-    .addColumn("new_hardware_id", "text", (col) =>
-      col.references("hardware.id").onDelete("cascade").notNull(),
-    )
+    ) // this is the hardware that you are doing the revision on
+    .addColumn("revision_type", sql`revision_type`, (col) => col.notNull())
     .addColumn("created_at", "timestamp", (col) =>
-      col.defaultTo(sql`now()`).notNull(),
+      col.notNull().defaultTo(sql`now()`),
     )
-    .addPrimaryKeyConstraint("revision_pk", [
-      "old_hardware_id",
-      "new_hardware_id",
-    ])
+    .addColumn("component_id", "text", (col) =>
+      col.references("hardware.id").onDelete("cascade").notNull(),
+    ) // this is the component that you initialized/added/removed.
+    .addColumn("reason", "text", (col) => col)
+    .addColumn("user_id", "text", (col) => col.references("user.id").notNull())
     .execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
   await db.schema.dropTable("hardware_revision").execute();
+  await db.schema.dropType("revision_type").execute();
 }
