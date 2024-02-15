fe897dbb9b15b19e6dc685b728d78677294ba246
smahmed776 <xsmahmed776@gmail.com>
2024-02-15 20:03:59 +0000

    refactor: parsing measurement data

--- zapier/src/utils.ts
+++ zapier/src/utils.ts
@@ -1,10 +1,15 @@
 import { MeasurementInputData } from "./types/measurement";
 
+const dataParseMethods: {
+  [key in MeasurementInputData["type"]]: (data: string) => any;
+} = {
+  boolean: (data) => (typeof data !== "undefined" ? Boolean(data) : null),
+  dataframe: (data) => JSON.parse(data) as Record<string, number[] | string[]>,
+};
+
 export const getMeasurementValueByType = (
   data: string,
   type: MeasurementInputData["type"],
 ) => {
-  return type === "boolean"
-    ? Boolean(data)
-    : (JSON.parse(data) as Record<string, number[] | string[]>);
+  return dataParseMethods[type](data);
 };
