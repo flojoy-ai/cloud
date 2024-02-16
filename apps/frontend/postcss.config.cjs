cafb06f4bfcc12605e6ae81a82c560471d19265d
Joey Yu <joey@jyu.dev>
2024-02-16 13:57:20 -0500

    chore: hum why css broken

--- apps/frontend/postcss.config.cjs
+++ apps/frontend/postcss.config.cjs
@@ -1,8 +1,6 @@
-const { join } = require("path");
-
 const config = {
   plugins: {
-    tailwindcss: { config: join(__dirname, "tailwind.config.ts") },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
--- tailwind.config.base.ts
+++ tailwind.config.base.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.tsx"],
+  content: [],
   prefix: "",
   theme: {
     container: {
