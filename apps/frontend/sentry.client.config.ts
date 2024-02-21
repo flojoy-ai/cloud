90e3dffda69a9439de4c037802b9fa9b20d37fb0
Joey Yu <joey@jyu.dev>
2024-02-20 20:44:35 -0500

    chore: only enable sentry on prod

--- apps/frontend/sentry.client.config.ts
+++ apps/frontend/sentry.client.config.ts
@@ -3,28 +3,31 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { env } from "~/env";
 
-Sentry.init({
-  dsn: "https://11e5ec379ac80034bffdefcd3b8555cd@o4504914175131648.ingest.sentry.io/4506775599251456",
+if (env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://11e5ec379ac80034bffdefcd3b8555cd@o4504914175131648.ingest.sentry.io/4506775599251456",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    Sentry.replayIntegration({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
-});
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      Sentry.replayIntegration({
+        // Additional Replay configuration goes in here, for example:
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
+  });
+}
--- apps/frontend/sentry.edge.config.ts
+++ apps/frontend/sentry.edge.config.ts
@@ -4,13 +4,16 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { env } from "~/env";
 
-Sentry.init({
-  dsn: "https://11e5ec379ac80034bffdefcd3b8555cd@o4504914175131648.ingest.sentry.io/4506775599251456",
+if (env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://11e5ec379ac80034bffdefcd3b8555cd@o4504914175131648.ingest.sentry.io/4506775599251456",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
