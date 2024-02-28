466d5aa7d23ef88b117c322027d21144864bec64
Guillaume <g.thibault@polymtl.ca>
2024-02-28 12:07:26 -0500

    Adding test sequencer export

---
+++ sdks/python/flojoy-cloud/tests/sequencer_export_test.py
@@ -0,0 +1,84 @@
+from flojoy_cloud import test_sequencer
+from random import randint, random
+import pandas as pd
+
+
+def test_set_output():
+    # Set the output location to simulate a test run in the test sequencer
+    test_id = f"my_test_id_{randint(0,1000)}"
+    test_sequencer._set_output_loc(test_id)
+
+    # Simulate user export
+    my_value = randint(0, 1000)
+    test_sequencer.export(my_value)
+
+    # Change the output to default
+    test_sequencer._set_output_loc(None)
+    my_new_value = randint(0, 1000)
+    test_sequencer.export(my_new_value)
+
+    # Verify nothing was been lost
+    data = test_sequencer.get_most_recent_data(test_id)
+    assert data == my_value
+
+    data = test_sequencer.get_most_recent_data(None)
+    assert data == my_new_value
+
+
+def test_multiple_data_export():
+    """Only the lastest should be retreive"""
+    test_id = f"my_test_id_{randint(0,1000)}"
+    test_sequencer._set_output_loc(test_id)
+
+    # Simulate user export
+    my_value = randint(0, 1000)
+    test_sequencer.export(my_value)
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    test_sequencer.export(df)
+    my_new_value = randint(0, 1000)
+    test_sequencer.export(my_new_value)
+
+    # Verify that the lastest data is the only one retreive
+    data = test_sequencer.get_most_recent_data(test_id)
+    assert data == my_new_value
+
+
+def test_export_dataframe():
+    test_id = f"my_test_id_{randint(0,1000)}"
+    test_sequencer._set_output_loc(test_id)
+    # Simulate user export
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    test_sequencer.export(df)
+    # Verify that the lastest data is the only one retreive
+    data: pd.DataFrame = test_sequencer.get_most_recent_data(test_id)  # type: ignore
+    assert data.equals(df)
+
+
+def test_export_bool():
+    test_id = f"my_test_id_{randint(0,1000)}"
+    test_sequencer._set_output_loc(test_id)
+    # Simulate user export
+    test_sequencer.export(True)
+    # Verify that the lastest data is the only one retreive
+    data = test_sequencer.get_most_recent_data(test_id)
+    assert data is True
+
+
+def test_export_int():
+    test_id = f"my_test_id_{randint(0,1000)}"
+    test_sequencer._set_output_loc(test_id)
+    # Simulate user export
+    test_sequencer.export(1)
+    # Verify that the lastest data is the only one retreive
+    data = test_sequencer.get_most_recent_data(test_id)
+    assert data == 1
+
+
+def test_export_float():
+    test_id = f"my_test_id_{random()}"
+    test_sequencer._set_output_loc(test_id)
+    # Simulate user export
+    test_sequencer.export(1.0)
+    # Verify that the lastest data is the only one retreive
+    data = test_sequencer.get_most_recent_data(test_id)
+    assert data == 1.0
