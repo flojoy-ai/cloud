8436c49b12355a95f0d26f67245967d4166267ef
JeffDotPng <zhangjeff2014@gmail.com>
2024-03-18 14:34:29 -0400

    feat: minimalist home page for workspace

--- apps/platform/src/components/protected-nav.tsx
+++ apps/platform/src/components/protected-nav.tsx
@@ -45,15 +45,18 @@ export function ProtectedNav({ workspaces }: Props) {
   const namespace = segments[2];
 
   const currentWorkspace = workspaces.find((ws) => ws.namespace === namespace);
+  const workspaceHome = namespace !== undefined && !segments[3];
 
   return (
     <div className="mr-4 hidden md:flex">
-      <Link href="/" className="mr-6 flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
-      </Link>
+      {!workspaceHome && (
+        <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Icons.logo className="h-6 w-6" />
+          <span className="hidden font-bold sm:inline-block">
+            {siteConfig.name}
+          </span>
+        </Link>
+      )}
 
       <nav className="flex items-center gap-6 text-sm">
         <Popover open={open} onOpenChange={setOpen}>
