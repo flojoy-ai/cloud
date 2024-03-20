c52a4ca1b821da1b55edf1154c46c20b247caefb
Joey Yu <joey@jyu.dev>
2024-03-20 19:36:37 -0400

    fix: wrong import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import "index.css";
+import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
