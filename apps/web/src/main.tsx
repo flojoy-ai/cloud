f0fc1c68b652ab223b8a63392af02db5db263a95
Joey Yu <joey@jyu.dev>
2024-03-20 19:35:22 -0400

    chore: shadcn setup with geist font

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import "@cloud/ui/styles/globals.css";
+import "index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
