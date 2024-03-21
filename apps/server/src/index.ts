650a6fe08844f8b8e4d7d4bb0f9c447d889d5699
Joey Yu <joey@jyu.dev>
2024-03-21 15:28:03 -0400

    chore: move back to 3000

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -9,7 +9,7 @@ const app = new Elysia()
   .use(userRoute)
   .use(authRoute)
   .use(workspacesRoute)
-  .listen(3001);
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
