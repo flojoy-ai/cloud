2790c0b955c683e8daf865e1ab0472def3c6e637
Joey Yu <joey@jyu.dev>
2024-03-20 20:02:26 -0400

    chore: t3-env setup

--- apps/web/src/lib/client.ts
+++ apps/web/src/lib/client.ts
@@ -1,4 +1,5 @@
+import { env } from "@/env";
 import type { App } from "@cloud/server/src/index";
 import { treaty } from "@elysiajs/eden";
 
-export const client = treaty<App>("http://localhost:3001");
+export const client = treaty<App>(env.VITE_SERVER_URL);
