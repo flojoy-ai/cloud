d78b298fd8d1e9a1e7a990974a7ecc43b37ab05a
Joey Yu <joey@jyu.dev>
2024-03-21 15:37:25 -0400

    feat: logout route

--- apps/server/src/routes/auth.ts
+++ apps/server/src/routes/auth.ts
@@ -1,11 +1,21 @@
 import { lucia } from "@/auth/lucia";
+import { env } from "@/env";
 import { AuthMiddleware } from "@/middlewares/auth";
 import { Elysia } from "elysia";
 
 export const authRoute = new Elysia({ prefix: "/auth" })
   .use(AuthMiddleware)
-
-  .get("/logout", async ({ set, session }) => {
+  .get("/logout", async ({ set, session, cookie }) => {
+    // make sure to invalidate the current session!
     await lucia.invalidateSession(session.id);
+
     const sessionCookie = lucia.createBlankSessionCookie();
+
+    cookie[sessionCookie.name]?.set({
+      value: sessionCookie.value,
+      ...sessionCookie.attributes,
+    });
+
+    set.redirect = env.WEB_URL;
+    return;
   });
