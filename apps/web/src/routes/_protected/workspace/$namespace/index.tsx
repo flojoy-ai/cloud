f7b03baf9af834fc10149a399fa565c678bd0b48
Joey Yu <joey@jyu.dev>
2024-03-24 12:42:26 -0400

    style: lots of css cleanup

--- apps/web/src/routes/_protected/workspace/$namespace/index.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/index.tsx
@@ -1,10 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { Link } from "@tanstack/react-router";
-import { Icons } from "@/components/icons";
-import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import SearchBar from "@/components/workspace/search-bar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const Route = createFileRoute("/_protected/workspace/$namespace/")({
   component: Page,
@@ -15,13 +14,24 @@ function Page() {
   const { workspace } = Route.useRouteContext();
 
   return (
-    <div className="container flex h-[calc(100vh-56px)] max-w-screen-2xl justify-center items-center">
-      <div className="mb-48 flex w-[512px] flex-col items-center">
-        <div className="mx-auto flex w-fit items-center space-x-3">
-          <Icons.logo className="h-12 w-12" />
-          <span className="text-3xl font-bold">{siteConfig.name}</span>
+    <div className="container max-w-screen-2xl w-full justify-center items-center">
+      <div className="flex flex-col items-center">
+        <div className="py-8"></div>
+
+        <div className="flex items-center space-x-3">
+          <Avatar className="h-8 w-8">
+            <AvatarImage
+              src={`https://avatar.vercel.sh/${workspace.name}.png`}
+              alt={workspace.name}
+              className="grayscale"
+            />
+            <AvatarFallback>FJ</AvatarFallback>
+          </Avatar>
+          <span className="text-3xl font-bold">{workspace.name}</span>
         </div>
+
         <div className="py-4" />
+
         <div className="flex gap-x-6">
           <Link
             to={`/workspace/$namespace/project`}
@@ -52,11 +62,11 @@ function Page() {
             Workspace Settings
           </Link>
         </div>
+
         <div className="py-2" />
-        <div className="relative w-full">
-          <div className="absolute w-full">
-            <SearchBar workspaceId={workspace.id} namespace={namespace} />
-          </div>
+
+        <div className="w-[512px]">
+          <SearchBar workspaceId={workspace.id} namespace={namespace} />
         </div>
       </div>
 
