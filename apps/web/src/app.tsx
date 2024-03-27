13dc08ae6af2e0adce60ae1eb5b701902da46a66
Joey Yu <joey@jyu.dev>
2024-03-27 11:59:58 -0400

    fix: disbale defaultPreload

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -10,13 +10,14 @@ const router = createRouter({
     queryClient,
     auth: undefined!,
   },
-  defaultPreload: "intent",
-  // Since we're using React Query, we don't want loader calls to ever be stale
-  // This will ensure that the loader is always called when the route is preloaded or visited
-  // defaultPreloadStaleTime: 0,
+
   // FIXME: Joey: comment this out for now, since this is a router bug
   // https://github.com/TanStack/router/issues/1201
   // https://github.com/TanStack/router/issues/1162
+  // defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  // defaultPreloadStaleTime: 0,
 });
 
 // Register the router instance for type safety
