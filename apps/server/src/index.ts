1261bae8bc6fe84e06b66ab9410965cd94654f92
Joey Yu <joey@jyu.dev>
2024-03-27 22:02:41 -0400

    chore: ok

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -71,7 +71,7 @@ const app = new Elysia()
   .use(StationRoute)
   .use(HardwareRoute)
   .use(SessionRoute)
-  .get("/health", () => ({ status: "ok" }))
+  .get("/health", () => "ok")
   .listen(env.PORT);
 
 console.log(
