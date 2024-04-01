c7e8bb50523622e474ae168c6ba5055e7bdb6f6c
Joey Yu <joey@jyu.dev>
2024-04-01 15:41:58 -0400

    chore: add an error component

--- apps/web/src/routes/_protected/workspace/$namespace/route.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/route.tsx
@@ -1,8 +1,27 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
 import { getWorkspaceQueryOpts } from "@/lib/queries/workspace";
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_protected/workspace/$namespace")({
   component: Page,
+
+  errorComponent: () => (
+    <PageHeader>
+      <PageHeaderHeading>Oops!</PageHeaderHeading>
+      <PageHeaderDescription>
+        You do not have access to this workspace :(
+      </PageHeaderDescription>
+      <div className="py-2"></div>
+      <Button asChild>
+        <Link to={"/workspace"}>Go Back</Link>
+      </Button>
+    </PageHeader>
+  ),
   beforeLoad: async ({ context: { queryClient }, params: { namespace } }) => {
     const workspace = await queryClient.ensureQueryData(
       getWorkspaceQueryOpts({ namespace }),
