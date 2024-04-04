74e19a13dd9b615fdc0ce221a3702ba74d855d35
Joey Yu <joey@jyu.dev>
2024-04-04 15:34:55 -0400

    Revert "fix: secure search query"
    This reverts commit e1b42d6516545c349b0e84f08dcd0e34d02f37a3.

--- apps/server/src/routes/search.ts
+++ apps/server/src/routes/search.ts
@@ -1,4 +1,4 @@
-import { SearchResult, WorkspaceUser, searchInput } from "@cloud/shared";
+import { SearchResult, searchInput } from "@cloud/shared";
 import Elysia from "elysia";
 import { SqlBool, sql } from "kysely";
 import { db } from "../db/kysely";
@@ -8,33 +8,19 @@ import { SearchResultType } from "@cloud/shared";
 function makeQuery(
   tableName: SearchResultType,
   query: string,
-  workspaceUser: WorkspaceUser,
+  workspaceId: string,
 ) {
   switch (tableName) {
     case "product":
     case "part":
-      return db
-        .selectFrom(tableName)
-        .select(["name", "id"])
-        .select(sql<SearchResult["type"]>`${tableName}`.as("type"))
-        .select(sql<number>`name <-> ${query}`.as("dist"))
-        .where(sql<SqlBool>`(name <-> ${query}) < 0.85`)
-        .where("workspaceId", "=", workspaceUser.workspaceId);
     case "project":
       return db
         .selectFrom(tableName)
         .select(["name", "id"])
-        .innerJoin("project_user as pu", (join) =>
-          join
-            .on("pu.workspaceId", "=", workspaceUser.workspaceId)
-            .on("pu.userId", "=", workspaceUser.userId)
-            .onRef("pu.projectId", "=", "project.id"),
-        )
-        .where("pu.role", "!=", "pending")
         .select(sql<SearchResult["type"]>`${tableName}`.as("type"))
         .select(sql<number>`name <-> ${query}`.as("dist"))
         .where(sql<SqlBool>`(name <-> ${query}) < 0.85`)
-        .where("workspaceId", "=", workspaceUser.workspaceId);
+        .where("workspaceId", "=", workspaceId);
     case "partVariation":
       return db
         .selectFrom("part_variation")
@@ -42,7 +28,7 @@ function makeQuery(
         .select(sql<SearchResult["type"]>`'partVariation'`.as("type"))
         .select(sql<number>`part_variation.part_number <-> ${query}`.as("dist"))
         .where(sql<SqlBool>`(part_variation.part_number <-> ${query}) < 0.85`)
-        .where("workspaceId", "=", workspaceUser.workspaceId);
+        .where("workspaceId", "=", workspaceId);
     case "unit":
       return db
         .selectFrom("unit")
@@ -50,7 +36,7 @@ function makeQuery(
         .select(sql<SearchResult["type"]>`'unit'`.as("type"))
         .select(sql<number>`unit.serial_number <-> ${query}`.as("dist"))
         .where(sql<SqlBool>`(unit.serial_number <-> ${query}) < 0.85`)
-        .where("workspaceId", "=", workspaceUser.workspaceId);
+        .where("workspaceId", "=", workspaceId);
   }
 }
 
@@ -61,25 +47,21 @@ export const SearchRoute = new Elysia({
   .use(WorkspaceMiddleware)
   .get(
     "/",
-    async ({ query: queryParams, workspaceUser, error }) => {
+    async ({ query: queryParams, workspace }) => {
       if (queryParams.query.length === 0) {
         return [];
       }
-      if (workspaceUser.role === "pending") {
-        return error("Forbidden");
-      }
-
       const { query } = queryParams;
 
-      const productQuery = makeQuery("product", query, workspaceUser);
-      const partQuery = makeQuery("part", query, workspaceUser);
+      const productQuery = makeQuery("product", query, workspace.id);
+      const partQuery = makeQuery("part", query, workspace.id);
       const partVariationQuery = makeQuery(
         "partVariation",
         query,
-        workspaceUser,
+        workspace.id,
       );
-      const unitQuery = makeQuery("unit", query, workspaceUser);
-      const projectQuery = makeQuery("project", query, workspaceUser);
+      const unitQuery = makeQuery("unit", query, workspace.id);
+      const projectQuery = makeQuery("project", query, workspace.id);
 
       const searchQuery = [
         productQuery,
