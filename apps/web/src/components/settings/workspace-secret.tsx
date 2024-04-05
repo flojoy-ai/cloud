81bd8bb632fe43e32c33bf2bd89c2171d772ee1e
Joey Yu <joey@jyu.dev>
2024-04-05 14:16:37 -0400

    chore: better looking secret page

--- apps/web/src/components/settings/workspace-secret.tsx
+++ apps/web/src/components/settings/workspace-secret.tsx
@@ -4,6 +4,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/client";
 import { getSecretQueryKey } from "@/lib/queries/secret";
 import { UserSession } from "@cloud/shared/src/schemas/public/UserSession";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { toast } from "sonner";
 
 type Props = {
   workspace: Workspace;
@@ -28,15 +31,57 @@ const WorkspaceSecret = ({ workspace, secret }: Props) => {
   }
 
   return (
-    <div className="">
-      <Button onClick={handleGenerate}>Generate</Button>
-      {secret && (
-        <Button onClick={() => navigator.clipboard.writeText(secret.id)}>
-          Copy
+    <div className="space-y-8">
+      <div className="grid w-full max-w-lg items-center gap-1.5">
+        <Label htmlFor="workspace_id">Workspace ID</Label>
+        <div className="flex items-center gap-2">
+          <Input
+            type="text"
+            id="workspace_id"
+            disabled
+            placeholder={workspace.id}
+          />
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(workspace.id);
+              toast.success("Copied to clipboard");
+            }}
+          >
+            Copy
+          </Button>
+        </div>
+      </div>
+
+      <div className="grid w-full max-w-lg items-center gap-1.5">
+        <Label htmlFor="workspace_personal_secret">
+          Workspace Personal Secret
+        </Label>
+
+        <div className="flex items-center gap-2">
+          {secret && (
+            <>
+              <Input
+                type="text"
+                id="workspace_personal_secret"
+                disabled
+                placeholder={secret.id}
+                className="text-ellipsis"
+              />
+              <Button
+                onClick={() => {
+                  navigator.clipboard.writeText(secret.id);
+                  toast.success("Copied to clipboard");
+                }}
+              >
+                Copy
+              </Button>
+            </>
+          )}
+        </div>
+        <Button onClick={handleGenerate}>
+          {secret ? "Regenerate" : "Generate"}
         </Button>
-      )}
-      <div className="whitespace-pre-wrap break-words">{secret?.id}</div>
-      <div className="whitespace-pre-wrap break-words">{workspace.id}</div>
+      </div>
     </div>
   );
 };
