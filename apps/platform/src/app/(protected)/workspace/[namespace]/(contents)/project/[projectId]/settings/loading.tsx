665e118f385dabef93240d1177ae2f1abd22fa52
Joey Yu <joey@jyu.dev>
2024-04-11 09:44:54 -0400

    chore: nuke the nextjs app

--- apps/platform/src/app/(protected)/workspace/[namespace]/(contents)/project/[projectId]/settings/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { Icons } from "~/components/icons";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <Icons.spinner className="mx-auto animate-spin" />;
-}
--- apps/platform/src/app/(public)/privacy/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function PrivacyPage() {
-  return <div>Privacy Page</div>;
-}
