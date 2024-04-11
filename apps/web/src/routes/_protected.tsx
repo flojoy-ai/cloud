522cb110258c45939f8b2271e3b102ca1030522a
Joey Yu <joey@jyu.dev>
2024-04-11 15:06:05 -0400

    fix: un-auth redirect

--- apps/web/src/routes/_protected.tsx
+++ apps/web/src/routes/_protected.tsx
@@ -2,19 +2,24 @@ import CenterLoadingSpinner from "@/components/center-loading-spinner";
 import { ProtectedHeader } from "@/components/navbar/protected-header";
 import { getWorkspacesQueryOpts } from "@/lib/queries/workspace";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+import {
+  Navigate,
+  Outlet,
+  createFileRoute,
+  redirect,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected")({
   component: Protected,
-  beforeLoad({ context }) {
-    if (!context.auth.isLoading && !context.auth.user) {
+  loader: ({ context }) => {
+    try {
+      context.queryClient.ensureQueryData(getWorkspacesQueryOpts());
+    } catch (error) {
       throw redirect({ to: "/login" });
     }
   },
-  loader: ({ context }) => {
-    context.queryClient.ensureQueryData(getWorkspacesQueryOpts());
-  },
   pendingComponent: CenterLoadingSpinner,
+  errorComponent: () => <Navigate to="/login" />,
 });
 
 function Protected() {
