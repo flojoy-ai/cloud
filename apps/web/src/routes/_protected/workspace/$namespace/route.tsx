28fc965b68add64ce2d533d1e550c07c1d23e8dd
Joey Yu <joey@jyu.dev>
2024-04-12 10:10:24 -0400

    fix: ambiguous query

--- apps/web/src/routes/_protected/workspace/$namespace/route.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/route.tsx
@@ -14,18 +14,21 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 export const Route = createFileRoute("/_protected/workspace/$namespace")({
   component: Page,
 
-  errorComponent: () => (
-    <PageHeader>
-      <PageHeaderHeading>Oops!</PageHeaderHeading>
-      <PageHeaderDescription>
-        You do not have access to this workspace :(
-      </PageHeaderDescription>
-      <div className="py-2"></div>
-      <Button asChild>
-        <Link to={"/workspace"}>Go Back</Link>
-      </Button>
-    </PageHeader>
-  ),
+  errorComponent: (error) => {
+    console.error(error);
+    return (
+      <PageHeader>
+        <PageHeaderHeading>Oops!</PageHeaderHeading>
+        <PageHeaderDescription>
+          You do not have access to this workspace :(
+        </PageHeaderDescription>
+        <div className="py-2"></div>
+        <Button asChild>
+          <Link to={"/workspace"}>Go Back</Link>
+        </Button>
+      </PageHeader>
+    );
+  },
 
   pendingComponent: CenterLoadingSpinner,
   beforeLoad: async ({ context: { queryClient }, params: { namespace } }) => {
