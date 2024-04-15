a65f738c477e042b303ef78f4ec5c2506e2b623e
Joey Yu <joey@jyu.dev>
2024-04-15 11:32:58 -0400

    fix: oops put the provider in the wrong file

--- apps/web/src/routes/_protected/workspace/$namespace/project/$projectId/route.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/project/$projectId/route.tsx
@@ -1,6 +1,9 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { getProjectQueryOpts } from "@/lib/queries/project";
 import CenterLoadingSpinner from "@/components/center-loading-spinner";
+import { getProjectUserQueryOpts } from "@/lib/queries/user";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ProjectUserProvider } from "@/context/project-user";
 
 export const Route = createFileRoute(
   "/_protected/workspace/$namespace/project/$projectId",
@@ -12,9 +15,24 @@ export const Route = createFileRoute(
     );
     return { project };
   },
+  loader: ({ context, params: { projectId } }) => {
+    context.queryClient.ensureQueryData(
+      getProjectUserQueryOpts({ projectId, context }),
+    );
+  },
   component: Page,
 });
 
 function Page() {
-  return <Outlet />;
+  const { workspace } = Route.useRouteContext();
+  const { projectId } = Route.useParams();
+
+  const { data: projectUser } = useSuspenseQuery(
+    getProjectUserQueryOpts({ projectId, context: { workspace } }),
+  );
+  return (
+    <ProjectUserProvider projectUser={projectUser}>
+      <Outlet />
+    </ProjectUserProvider>
+  );
 }
